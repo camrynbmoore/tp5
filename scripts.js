6679f73a490909328ce913a2fdadcac9f7bc46e4
Camryn <camryn.moore@wsu.edu>
2022-09-27 02:41:16 -0400

    Created .js file, copied html and css from tp4, started tp5. Some code was not diplaying when I refreshed.

---
+++ scripts.js
@@ -0,0 +1,15 @@
+// JavaScript window.onload
+
+window.onload = function() {
+ document.querySelector("#heroimage h1").classList.add("tp");
+ 
+  document.quesrySelector("#heroimage h1").onclick = function() {
+    this.classList.toggle("lightgreen");   
+  }
+  
+} // end window.onload
+
+
+//button
+
+//button .onclick="getElementByTagName(h4)"
